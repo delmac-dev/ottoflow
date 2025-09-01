398cba7838a619c052f5c87c5679a2328702d6b1
Delali Tengue <delalitengue.dev@gmail.com>
2025-09-01 19:26:12 +0000

    integrate vercel ai sdk

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -1,26 +1,34 @@
-import { GoogleGenAI } from "@google/genai";
 import { NextRequest, NextResponse } from "next/server";
+import { generateObject, generateText } from 'ai';
+import { google } from '@ai-sdk/google';
+import z from "zod";
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     const { fileUrl, prompt } = body;
 
-    // const ai = new GoogleGenAI({});
-
-    // const response = await ai.models.generateContent({
-    //   model: "gemini-2.5-flash",
-    //   contents: "what is the capital of ghana",
-    //   config: {
-    //     thinkingConfig: {
-    //       thinkingBudget: 0,
-    //     },
-    //   },
+    // const { text } = await generateText({
+    //   model: google('gemini-2.5-flash'),
+    //   prompt: 'Write a vegetarian lasagna recipe for 4 people.',
     // });
 
-    // const output = response.text;
+    const result = await generateObject({
+      model: google('gemini-2.5-flash'),
+      schema: z.object({
+        events: z.array(
+          z.object({
+            event: z.string(),
+            date: z.string()
+          }),
+        ),
+      }),
+      prompt: 'List 5 important events from the year 2000.',
+    });
 
-    return NextResponse.json({ message: "Success", data: { fileUrl, prompt }});
+    console.log("Generated Result:", result);
+
+    return NextResponse.json({ message: "Success", data: result.object.events });
   } catch (error: any) {
     console.error("API Error:", error);
     return NextResponse.json({ error: "Something went wrong", details: error.message },{ status: 500 });
