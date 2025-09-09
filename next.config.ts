d1d42e006dce9d64b6784a27ff64a8b6ca14ec0b
Delali Tengue <delalitengue.dev@gmail.com>
2025-09-09 12:55:14 +0000

    make progress

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
+  webpack: (config) => {
+    config.resolve.alias["canvas"] = path.resolve("./empty.ts");
+    return config;
   },
   turbopack: {
     resolveAlias: {
