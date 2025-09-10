1642fb0d4638b87ec551f71c46cbc9e43c92f6c0
Delali Tengue <delalitengue.dev@gmail.com>
2025-09-10 12:27:13 +0000

    edit utils baseUrl

--- lib/utils.ts
+++ lib/utils.ts
@@ -22,7 +22,7 @@ export function cn(...inputs: ClassValue[]) {
 export const _baseUrl = () => {
 
   if(process.env.NEXT_PUBLIC_VERCEL_ENV === "production")
-    return (`https://${process.env.NEXT_PUBLIC_VERCEL_URL}`)
+    return (`https://${process.env.VERCEL_URL}`)
   else
     return (`http://${process.env.NEXT_PUBLIC_VERCEL_URL}`)
 
